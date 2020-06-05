f7e33fafb5931d746227d56a078a4cd7af738dbe
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-06-05 18:12:53 +0200

    Fix Conflict

--- app/src/main/java/com/ljmu/andre/snaptools/Utils/PackUtils.kt
+++ app/src/main/java/com/ljmu/andre/snaptools/Utils/PackUtils.kt
@@ -7,7 +7,7 @@ import com.ljmu.andre.snaptools.data.PackMetadata
  * This file was created by Jacques Hoffmann (jaqxues) in the Project SnapTools.<br>
  * Date: 02.06.20 - Time 21:00.
  */
-object PackUtilsKt {
+object PackUtilsKt2 {
     fun getFlavourText(flavour: String) =
             when (flavour) {
                 "prod" -> "Release"
@@ -17,4 +17,4 @@ object PackUtilsKt {
 }
 
 val PackMetadata.displayName get() =
-    (if (flavour == "prod") "" else PackUtilsKt.getFlavourText(flavour)) + "Pack v" + scVersion
+    (if (flavour == "prod") "" else PackUtilsKt2.getFlavourText(flavour)) + "Pack v" + scVersion
